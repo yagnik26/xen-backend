d3d8bac64edea028127a93ebcd8c7e358dfc9bcf
yagnik <yagnikgoti26@gmail.com>
2023-10-27 17:32:00 +0530

    added / path

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.use("/", ltpRouter);
+app.get("/", (req, res) => {
+  res.send("working....");
+});
+app.use("/api", ltpRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("listening on port " + process.env.PORT);
